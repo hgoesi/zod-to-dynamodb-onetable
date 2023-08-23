d773fe339168f81025224871881b0e4de104d942
Johannes Konings <johannes.konings@outlook.com>
2023-08-23 20:32:25 +0200

    chore: init

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,15 @@
-import { typescript } from 'projen';
+import { javascript, typescript } from 'projen';
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'zod-to-dynamodb-onetable',
   projenrcTs: true,
+  packageManager: javascript.NodePackageManager.NPM,
+  releaseToNpm: true,
+  npmAccess: javascript.NpmAccess.PUBLIC,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  packageName: '@hgoesi/zod-to-dynamodb-onetable',
 });
 project.synth();
